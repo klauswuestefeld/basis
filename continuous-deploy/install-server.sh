60998a7220d015135a5f2efb4924f8fbce1dc5c1
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2012-03-22 23:29:48 -0300

    Minor

--- continuous-deploy/install-server.sh
+++ continuous-deploy/install-server.sh
@@ -7,7 +7,7 @@ aptitude -y safe-update
 echo
 echo
 echo ---- Timezone
-# Choose yours here http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Choose your timezone: http://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 echo "America/Sao_Paulo" > /etc/timezone
 dpkg-reconfigure -f noninteractive tzdata
 
